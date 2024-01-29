860ec70b62dd74842941b773474804ee25751e53
07x <abhinavsrivastav421@gmail.com>
2024-01-29 18:09:04 +0530

    add: initial commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# React + Vite Application 
+
+>>>>>>>>>>
+## Beautiful Single Product Page
+
+<!-- 
+<!-- > ### React + Vite codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld-example-apps) spec and API. -->
+
+[RealWorld](https://github.com/gothinkster/realworld)&nbsp;&nbsp;&nbsp;&nbsp;[Demo](https://react-vite-realworld.netlify.app/)
+
+## Getting started
+
+To get the frontend running locally:
+
+- Clone this repo
+- `npm install` to install all req'd dependencies
+- `npm run dev` to start the local server (this project Vite)
+
+### Making requests to the backend API
+
+## Functionality overview
+
+The example application is a simple beautiful web page of a product 
+
+**General functionality:**
+
+**The general page breakdown looks like this:**
+<br />
+
+<!-- [![Brought to you by Thinkster](https://raw.githubusercontent.com/gothinkster/realworld/master/media/end.png)](https://thinkster.io) --> -->
---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+const HeroSection = () => {
+    return (
+
+        <main className="hero container">
+
+            {/*LEFT SECTION */}
+            <div className="hero-content">
+                <h1> YOUR FEET  DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES. </p>
+
+                {/* BUTTON */}
+                <div className="hero-btn">
+                    <button> Shop Now</button>
+                    <button className="secondary-btn"> Category</button>
+                </div>
+
+                {/* SHOP */}
+                <div className="shopping">
+                    <p>Also Available On </p>
+                </div>
+
+                {/* BRAND ICONS */}
+                <div className="brand-icons">
+                    <img src="./images/amazon.png" alt="amazon-logo" />
+                    
+                    <img src="./images/flipkart.png" alt="flipkart-logo"/>
+
+                </div>
+            </div>
+
+            {/* RIGHT SECTION */}
+            <div className="hero-image">
+                <img src="./images/shoe_image.png" alt="hero-image"/>
+            </div>
+
+
+        </main>
+
+    );
+};
+
+export default HeroSection 
